6e84f8227266b9cb91dda62153937b2f84d43f05
slyflare <vimal.g.132000@gmail.com>
2020-05-09 17:35:09 -0400

    gui + ven

--- GUI.py
+++ GUI.py
@@ -1,5 +1,19 @@
-import pygame
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
 
-import Formulas
+class testwindow(QMainWindow):
 
+    def __init__(self, *args, **kwargs):
+        super(testwindow, self).__init__(*args,**kwargs)
+        self.setWindowTitle("test string")
+        label = QLabel('bubjabksdjkabsdjkb')
+        label.setAlignment(Qt.AlignCenter)
+        self.setCentralWidget(label)
 
+app = QApplication(sys.argv)
+
+window = testwindow()
+window.show()
+
+app.exec_()

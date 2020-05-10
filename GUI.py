61a337dfe24d190c87ad4b18515bf326214063be
slyflare <vimal.g.132000@gmail.com>
2020-05-10 15:12:54 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,22 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 
 class testwindow(QMainWindow):
-
     def __init__(self, *args, **kwargs):
         super(testwindow, self).__init__(*args,**kwargs)
-        self.setWindowTitle("wha-")
-        label = QLabel('KORE GA REQUIEM DA')
+
+        self.setWindowTitle('Test')
+        self.setGeometry(0,0,200,500)
+        label = QLabel('Test', self)
         label.setAlignment(Qt.AlignCenter)
-        self.setCentralWidget(label)
+        label.setStyleSheet('border:2px solid black')
 
 
 def window():
     app = QApplication(sys.argv)
     win = testwindow()
-    win.setGeometry(0, 0, 500, 500)
     win.show()
     sys.exit(app.exec_())
 
